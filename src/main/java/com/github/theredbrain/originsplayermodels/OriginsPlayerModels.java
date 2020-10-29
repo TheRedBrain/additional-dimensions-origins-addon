a0f4cfbd2f311416775f80cabe22d4b15da52831
TheRedBrain <theredbrain@gmail.com>
2020-10-29 18:03:16 +0100

    finally fixed repo

--- src/main/java/com/github/theredbrain/originsplayermodels/OriginsPlayerModels.java
+++ src/main/java/com/github/theredbrain/originsplayermodels/OriginsPlayerModels.java
@@ -3,6 +3,9 @@ package com.github.theredbrain.originsplayermodels;
 import net.fabricmc.api.ModInitializer;
 
 public class OriginsPlayerModels implements ModInitializer {
+
+	public static final String MOD_ID = "originsplayermodels";
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
